fb97b30fa8f168090fd80cf0f25e6491bc2d29ec
White Lily <mmacalino03@gmail.com>
2018-07-15 03:45:44 -0400

    updated java and added hover feature

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,64 +1,55 @@
-var topics = ["Sailor Moon", "Naruto", "Legend of Zelda", "Pokemon", "Kingdom Hearts", "Final Fantasy", "Avatar the Last Airbender", "A Silent Voice", "Playstation", "Nintendo", "Cloud Strife", "Sherlock Holmes", "Star Wars", "Spirited Away", "Yoshi", "Persona 5", "Bob's Burgers", "Game of Thrones"];
+var topics = ["Sailor Moon", "Naruto", "Legend of Zelda", "Pokemon", "Kingdom Hearts", "Final Fantasy", "Avatar the Last Airbender", "A Silent Voice", "Playstation", "Nintendo", "Cloud Strife", "Sherlock Holmes", "Star Wars", "Spirited Away", "Yoshi", "Persona 5", "Bobs Burgers", "Game of Thrones"];
 
-$(document).ready(function() {
-    for (var i = 0; i < topics.length; i++) {
-        $("#nerd-button").append("<button type='button' onclick='searchGif(\"" + topics[i] + "\")' class='btn btn-primary' value=' " + topics[i] + "'> " + topics[i] + " </button>");
-    }
+var currentGif;
+var animateGif;
+var stillGif;
+var pauseGif;
+//creates buttons
+function renderButtons(){
+	$('#nerd-button').empty();
+	for(var i = 0; i < topics.length; i++){
+		var nerdBtn = $('<button>').text(topics[i]).addClass('nerdBtn').attr({'data-name': topics[i]});
+		$('#nerd-button').append(nerdBtn);
+	}
+
+	$('.nerdBtn').on('click', function(){
+		$('.display').empty();
+
+        var thisShow = $(this).data('name');
+		var giphyURL = "http://api.giphy.com/v1/gifs/search?q=" + thisShow + "&limit=10&api_key=dc6zaTOxFJmzC";
+		$.ajax({url: giphyURL, method: 'GET'}).done(function(giphy){
+			currentGif = giphy.data;
+			$.each(currentGif, function(index,value){
+				animateGif= value.images.original.url;
+				pauseGif = value.images.original_still.url;
+				var thisRating = value.rating;
+				
+				if(thisRating == ''){
+					thisRating = 'unrated';
+				}
+				var rating = $('<h5>').html('Rated: '+thisRating).addClass('ratingStyle');
+				stillGif= $('<img>').attr('data-animated', animateGif).attr('data-paused', pauseGif).attr('src', pauseGif).addClass('playOnHover');
+				var fullGifDisplay = $('<button>').append(rating, stillGif);
+				$('.display').append(fullGifDisplay);
+			});
+		});
+	});
+}
+
+//animates and pauses gif on hover
+$(document).on('mouseover','.playOnHover', function(){
+ 	   	$(this).attr('src', $(this).data('animated'));
+ });
+ $(document).on('mouseleave','.playOnHover', function(){
+ 	   	$(this).attr('src', $(this).data('paused'));
+ });
+
+//sets a button from input
+$('#addGif').on('click', function(){
+	var newGif = $('#nerdInput').val().trim();
+	topics.push(newGif);
+	createButtons();
+	return false;
 });
 
-function buttonClicked() {
-    var userInput = $('#nerd-input').val();
-    searchGif(userInput);
-}
-
-function submitButtonClicked() {
-    var userInput = $('#nerd-input').val();
-
-    // if (userInput) {
-    //     $("#nerd-button").append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " + userInput + "'> " + userInput + " </button>");
-    // }
-}
-
-function searchGif(gifName) {
-    $.ajax({
-            url: "https://api.giphy.com/v1/gifs/search?q= " + gifName + " &api_key=dc6zaTOxFJmzC&limit=12",
-            type: 'GET',
-        })
-        .done(function(response) {
-            displayGif(response);
-        })
-}
-
-function displayGif(response) {
-    $('.display').empty();
-    for (var i = 0; i < response.data.length; i++) {
-        var rating = "<div class='ratings'> Rating:  " + (response.data[i].rating) + " </div>";
-        var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url +
-            '" data-still=" ' + response.data[i].images.fixed_height_still.url +
-            ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="movImage" style= "width:250px; height:250px;">';
-
-        image = '<div class="col-md-4">' + image + "</div>";
-        $('.display').append(image);
-    }
-
-    $(".movImage").on("click", function() {
-        var state = $(this).attr("data-state");
-        if (state == "still") {
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr('data-state', 'animate');
-        } else {
-            $(this).attr("src", $(this).attr("data-still"));
-            $(this).attr("data-state", "still");
-        }
-
-    });
-
-    $("#add-gif").on('click', function(event){
-        event.preventDefault();
-        var newNerd = $('#nerdInput').val().trim();
-        topics.push(newNerd);
-        renderButtons();
-    });
-
- 
-}
+renderButtons();
